910abc1b0c66ef2a96c79842ab300c2503d2d778
bella <yiseuljeong0415@gmail.com>
2025-04-18 20:12:48 +0900

    fix : 레벨 2-1: N+1 문제 EntityGraph 변환 완료

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,13 +1,21 @@
 package org.example.expert.domain.todo.repository;
 
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
+    @Override
     @EntityGraph(attributePaths = {"user"})
-    List<Todo> getTodos();
+    Page<Todo> findAll(Pageable pageable); // findAll + EntityGraph로 대체
+
+    @EntityGraph(attributePaths = {"user"})
+    Optional<Todo> findById(Long todoId); // findById + EntityGraph로 대체
+
+    int countById(Long todoId);
 }
