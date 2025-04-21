84770f450668cdf56cdb75168e858190af8941dd
bella <yiseuljeong0415@gmail.com>
2025-04-22 04:00:42 +0900

    feat : DTO 기본값 적용

--- src/main/java/org/example/expert/domain/comment/dto/request/CommentSaveRequest.java
+++ src/main/java/org/example/expert/domain/comment/dto/request/CommentSaveRequest.java
@@ -1,15 +1,15 @@
 package org.example.expert.domain.comment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class CommentSaveRequest {
 
-    @NotBlank
-    private String contents;
+    @NotBlank(message = "댓글 내용을 입력해주세요.")
+    private String contents = "";
+
+    public CommentSaveRequest(String contents) {
+        this.contents = contents;
+    }
 }
